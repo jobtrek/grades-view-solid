dc850a318d0b69940d013f5f858a7ae51e5b1a5e
fuzoh <info@bastiennicoud.ch>
2023-12-12 20:18:48 +0100

    add: modules numeros and descriptions datas

---
+++ src/data/cieModules.ts
@@ -0,0 +1,68 @@
+import { type Module } from '~/types/Module'
+
+export const cieModules: Module[] = [
+  {
+    no: 106,
+    name: 'Interroger traiter et assurer la maintenance des bases de données'
+  },
+  {
+    no: 107,
+    name: 'Mettre en oeuvre des solutions ICT avec la technologie block-chain'
+  },
+  {
+    no: 109,
+    name: 'Exploiter et surveiller des services dans le cloud public'
+  },
+  {
+    no: 110,
+    name: 'Analyser et repré-senter des données avec des outils'
+  },
+  {
+    no: 185,
+    name: 'Analyser et implé-menter des mesures visant à assurer la sécurité informa-tique des PME'
+  },
+  {
+    no: 187,
+    name: 'Mettre en service un Poste de tra-vail ICTavec le système d’exploitation'
+  },
+  {
+    no: 190,
+    name: 'Mettre en place et exploiter une plate-forme de virtualisa-tion'
+  },
+  {
+    no: 210,
+    name: 'Utiliser un cloud public pour des appli-cations'
+  },
+  {
+    no: 216,
+    name: 'Intégrer les ter-minaux IoE dans une plateforme existante'
+  },
+  {
+    no: 217,
+    name: 'Concevoir planifier et mettre en place un service pour l’IoE'
+  },
+  {
+    no: 223,
+    name: 'Réaliser des applica-tions multi-utilisateurs orien-tées objets'
+  },
+  {
+    no: 248,
+    name: 'Réaliser des solutions ICT avec des techno-logies actuelles'
+  },
+  {
+    no: 259,
+    name: 'Développer des solu-tions ICT avec le Ma-chine Learning'
+  },
+  {
+    no: 294,
+    name: 'Réaliser le front-end d’une application Web interactive'
+  },
+  {
+    no: 295,
+    name: 'Réaliser le back-end pour des applications'
+  },
+  {
+    no: 335,
+    name: 'Réaliser une appli-cation pour mobile'
+  }
+]
--- src/data/epsicModules.ts
+++ src/data/epsicModules.ts
@@ -2,15 +2,99 @@ import { type Module } from '~/types/Module'
 
 export const epsicModules: Module[] = [
   {
-    name: 'Mettre en oeuvre des systèmes de codification, de compression et d’encryptage',
-    no: 114
+    no: 114,
+    name: 'Mettre en oeuvre des systèmes de codification de compression et d’encryptage'
   },
   {
-    name: 'Mettre en place l’infrastructure informatique et réseau d’une petite entreprise',
-    no: 177
+    no: 117,
+    name: 'Mettre en place l’infrastructure informatique et réseau d’une petite entreprise'
   },
   {
-    name: 'Automatiser des procédures à l’aide de scripts',
-    no: 122
+    no: 122,
+    name: 'Automatiser des procédures à l’aide de scripts'
+  },
+  {
+    no: 162,
+    name: 'Analyser et modéliser des données'
+  },
+  {
+    no: 164,
+    name: 'Créer des bases de données et y insérer des données'
+  },
+  {
+    no: 165,
+    name: 'Utiliser des bases de données NoSQL'
+  },
+  {
+    no: 183,
+    name: 'Implémenter la sécurité d’une application'
+  },
+  {
+    no: 231,
+    name: 'Appliquer la protection et la sécurité des données'
+  },
+  {
+    no: 241,
+    name: 'Initialiser des solutions ICT innovantes'
+  },
+  {
+    no: 245,
+    name: 'Mettre en oeuvre des solutions ICT innovantes'
+  },
+  {
+    no: 254,
+    name: 'Décrire des processus métier dans son propre environnement professionnel'
+  },
+  {
+    no: 293,
+    name: 'Créer et publier un site Web'
+  },
+  {
+    no: 306,
+    name: 'Réaliser de petits projets dans son propre environnement professionnel'
+  },
+  {
+    no: 319,
+    name: 'Concevoir et implémenter des applications'
+  },
+  {
+    no: 320,
+    name: 'Programmer orienté objet'
+  },
+  {
+    no: 321,
+    name: 'Programmer des systèmes distribués'
+  },
+  {
+    no: 322,
+    name: 'Concevoir et implémenter des interfaces utilisateur'
+  },
+  {
+    no: 323,
+    name: 'Programmer de manière fonctionnelle'
+  },
+  {
+    no: 324,
+    name: 'Prendre en charge des processus DevOps avec des outils logiciels'
+  },
+  {
+    no: 346,
+    name: 'Concevoir et réaliser des solutions cloud'
+  },
+  {
+    no: 347,
+    name: 'Utiliser un service avec des conteneurs'
+  },
+  {
+    no: 426,
+    name: 'Développer un logiciel avec des méthodes agiles'
+  },
+  {
+    no: 431,
+    name: 'Exécuter des man-dats de manière autonome dans un environnement professionnel'
+  },
+  {
+    no: 450,
+    name: 'Tester des applications'
   }
 ]
