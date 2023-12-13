b3dc84e166783cdfeb62fe4a6bcfe5645c14164d
fuzoh <info@bastiennicoud.ch>
2023-12-13 09:17:35 +0100

    add: select button for navigation

--- src/components/AppNavigation.tsx
+++ src/components/AppNavigation.tsx
@@ -1,5 +1,6 @@
 import { type Component } from 'solid-js'
 import { A } from 'solid-start'
+import { SelectMenu } from '~/components/SelectMenu'
 
 export const AppNavigation: Component = () => {
   return (
@@ -95,23 +96,7 @@ export const AppNavigation: Component = () => {
               </nav>
             </div>
             <div class="col-span-1">
-              <div class="w-full">
-                <label for="search" class="sr-only">Search</label>
-                <div class="relative">
-                  <div
-                    class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                    <svg class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor"
-                         aria-hidden="true">
-                      <path fill-rule="evenodd"
-                            d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-                            clip-rule="evenodd"/>
-                    </svg>
-                  </div>
-                  <input id="search" name="search"
-                         class="block w-full rounded-md border-0 bg-sky-700 py-1.5 pl-10 pr-3 text-sky-100 placeholder:text-sky-500 focus:bg-white focus:text-gray-900 focus:ring-0 focus:placeholder:text-gray-500 sm:text-sm sm:leading-6"
-                         placeholder="Search" type="search"/>
-                </div>
-              </div>
+              <SelectMenu />
             </div>
           </div>
         </div>
