6bbbb7c3ab504115888cb5cd627a6f867e12de93
fuzoh <info@bastiennicoud.ch>
2024-01-20 12:55:34 +0100

    feat(import-export): Create a dedicated component for file import
    Signed-off-by: fuzoh <info@bastiennicoud.ch>

--- src/components/navigation/AppNavigation.tsx
+++ src/components/navigation/AppNavigation.tsx
@@ -6,6 +6,7 @@ import { NavButton } from "~/components/navigation/NavButton"
 import { resetGradesStore } from "~/contexts/gradesContext/setterUtils/resetGradesStore"
 import { useGradesContext } from "~/contexts/gradesContext/GradesContext"
 import { downloadObjectAsJson } from "~/utils/downloadObjectAsJson"
+import { FileButton } from "~/components/navigation/FileButton"
 
 const NameFormClient = clientOnly(
   async () => await import("~/components/forms/NameForm"),
@@ -33,21 +34,7 @@ export const AppNavigation: Component = () => {
           </div>
 
           <div class="hidden lg:ml-4 lg:flex lg:items-center lg:pr-0.5">
-            <NavButton name="Import" actionOnClick={() => {}}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                class="w-6 h-6"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M5.625 1.5H9a3.75 3.75 0 0 1 3.75 3.75v1.875c0 1.036.84 1.875 1.875 1.875H16.5a3.75 3.75 0 0 1 3.75 3.75v7.875c0 1.035-.84 1.875-1.875 1.875H5.625a1.875 1.875 0 0 1-1.875-1.875V3.375c0-1.036.84-1.875 1.875-1.875Zm5.845 17.03a.75.75 0 0 0 1.06 0l3-3a.75.75 0 1 0-1.06-1.06l-1.72 1.72V12a.75.75 0 0 0-1.5 0v4.19l-1.72-1.72a.75.75 0 0 0-1.06 1.06l3 3Z"
-                  clip-rule="evenodd"
-                />
-                <path d="M14.25 5.25a5.23 5.23 0 0 0-1.279-3.434 9.768 9.768 0 0 1 6.963 6.963A5.23 5.23 0 0 0 16.5 7.5h-1.875a.375.375 0 0 1-.375-.375V5.25Z" />
-              </svg>
-            </NavButton>
+            <FileButton />
             <NavButton
               name="Export"
               actionOnClick={() => {
