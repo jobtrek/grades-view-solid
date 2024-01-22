3922321cbc7aa2c54e87da806317da621a61c1f9
fuzoh <info@bastiennicoud.ch>
2024-01-22 16:28:42 +0100

    fix: Grades can now only be multiple of 0.5
    fixes: #13
    
    Signed-off-by: fuzoh <info@bastiennicoud.ch>

--- src/types/schemas/gradeFormSchema.ts
+++ src/types/schemas/gradeFormSchema.ts
@@ -1,4 +1,11 @@
-import { type Input, maxValue, minValue, number, object } from "valibot"
+import {
+  type Input,
+  maxValue,
+  minValue,
+  multipleOf,
+  number,
+  object,
+} from "valibot"
 
 const gradeSchemaLabels = {
   grade: "Note",
@@ -8,6 +15,7 @@ const AddGradeSchema = object({
   grade: number("La note doit être un nombre entre 1 et 6", [
     maxValue(6, "La note ne peut pas être supérieure à 6"),
     minValue(1, "La note ne peut pas être inférieure à 1"),
+    multipleOf(0.5, "La note ne peut être qu'un multiple de 0.5"),
   ]),
 } satisfies Record<keyof typeof gradeSchemaLabels, any>)
 
