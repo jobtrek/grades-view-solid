b772eab90a6e572392d077b4b2b8c8d2cd109696
fuzoh <info@bastiennicoud.ch>
2024-02-05 13:37:20 +0100

    fix: all pages now pre rendered
    Use explicit list of pages to prerender
    
    Signed-off-by: fuzoh <info@bastiennicoud.ch>

--- vite.config.ts
+++ vite.config.ts
@@ -6,22 +6,25 @@ export default defineConfig({
   },
   start: {
     server: {
-      preset: "github_pages",
+      preset: "github-pages",
       baseURL: "/grades-view-solid/",
+      compressPublicAssets: {
+        brotli: false,
+        gzip: false,
+      },
       prerender: {
         routes: [
           "/grades-view-solid/",
-          "/grades-view-solid/grades/math/",
-          "/grades-view-solid/grades/cie/",
-          "/grades-view-solid/grades/english/",
-          "/grades-view-solid/grades/epsic/",
-          "/grades-view-solid/grades/soci/",
+          "/grades-view-solid/grades/math",
+          "/grades-view-solid/grades/cie",
+          "/grades-view-solid/grades/english",
+          "/grades-view-solid/grades/epsic",
+          "/grades-view-solid/grades/soci",
         ],
         crawlLinks: true,
         failOnError: true,
         autoSubfolderIndex: true,
       },
     },
-    ssr: true,
   },
 })
