7190f1a142445e04571605280817dee233b3c936
fuzoh <info@bastiennicoud.ch>
2024-02-05 12:57:23 +0100

    fix: correct link to home on 404 page
    Signed-off-by: fuzoh <info@bastiennicoud.ch>

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -19,7 +19,7 @@ export default function (): JSX.Element {
               </p>
               <div class="mt-10 flex items-center justify-center gap-x-6">
                 <A
-                  href="#"
+                  href="/"
                   class="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Page d'accueil
