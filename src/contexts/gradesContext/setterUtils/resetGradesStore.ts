ae77bd185751af9871ef10ec460dd90b12e1a5b2
fuzoh <info@bastiennicoud.ch>
2024-02-06 09:54:09 +0100

    feat!: store app version in grade store
    this will allow to identify version of the store, and
    load the corresponding parser.
    
    BREAKING-CHANGE: new key on the store
    Closes: #46
    Signed-off-by: fuzoh <info@bastiennicoud.ch>

--- src/contexts/gradesContext/setterUtils/resetGradesStore.ts
+++ src/contexts/gradesContext/setterUtils/resetGradesStore.ts
@@ -1,11 +1,13 @@
 import { batch } from "solid-js"
 import { useGradesContext } from "~/contexts/gradesContext/GradesContext"
+import { initialGradesStoreData } from "~/data/initialGradesStoreData"
 
 const [, setGradesStore] = useGradesContext()
 export const resetGradesStore = (): void => {
   batch(() => {
-    setGradesStore("tpi", null)
-    setGradesStore("name", "")
+    setGradesStore("tpi", initialGradesStoreData.tpi)
+    setGradesStore("version", initialGradesStoreData.version)
+    setGradesStore("name", initialGradesStoreData.name)
     setGradesStore("info", "cie", [])
     setGradesStore("info", "epsic", [])
     setGradesStore("generalKnowledge", "eng", { semesters: [] })
