13c89300200c98d6008d7f46df88976cd4240ca2
fuzoh <info@bastiennicoud.ch>
2024-06-25 11:45:35 +0200

    chore(deps-dev): updates for flat eslint config

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+import js from "@eslint/js"
+import solid from "eslint-plugin-solid/configs/typescript.js"
+import * as tsParser from "@typescript-eslint/parser"
+import eslintConfigPrettier from "eslint-config-prettier"
+import eslintConfigLove from "eslint-config-love"
+import tseslint from "typescript-eslint"
+import globals from "globals"
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    ignores: [
+      "dist/**",
+      "node_modules/**",
+      "bin/**",
+      "build/**",
+      ".output/**",
+      ".vinxi/**",
+      "src/global.d.ts",
+      "eslint.config.mjs",
+      "postcss.config.cjs",
+      "tailwind.config.cjs",
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.js", "**/*.ts"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+    ...eslintConfigLove,
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    ...solid,
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "tsconfig.json",
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  eslintConfigPrettier,
+]
