fd3e6d3c2dc67f5864be6ef0f61a1fa7d996e18e
Bastien <bastien.nicoud@ik.me>
2024-08-14 07:33:07 +0200

    chore(main): release 2.0.19

--- src/data/initialGradesStoreData.ts
+++ src/data/initialGradesStoreData.ts
@@ -1,7 +1,7 @@
 import { type StudentGrades } from "~/types/models/GradeStoreModels"
 
 export const initialGradesStoreData = {
-  version: "2.0.18", // x-release-please-version
+  version: "2.0.19", // x-release-please-version
   name: "",
   tpi: null,
   info: {
